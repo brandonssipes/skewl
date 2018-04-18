dddd1c4275e0cd367c7dab63b0ff5d063c99b323
Sipes <m195922@usna.edu>
2018-04-18 13:53:31 -0400

    mmaping sutff

--- si335/proj2/part1.cpp
+++ si335/proj2/part1.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <cmath>
 
+//mmap
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
@@ -25,6 +26,7 @@ struct Node {
   int row;
   int col;
 };
+static uintmax_t wc(char const*);
 void push(struct Node**,int,int);
 void print_list(struct Node*);
 
@@ -126,3 +128,30 @@ void print_list(struct Node* cur){
     cur=cur->next;
   }
 }
+static uintmax_t wc(char const *fname)
+{
+    static const auto BUFFER_SIZE = 16*1024;
+    int fd = open(fname, O_RDONLY);
+    if(fd == -1)
+        handle_error("open");
+
+    /* Advise the kernel of our access pattern.  */
+    posix_fadvise(fd, 0, 0, 1);  // FDADVICE_SEQUENTIAL
+
+    char buf[BUFFER_SIZE + 1];
+    uintmax_t lines = 0;
+
+    while(size_t bytes_read = read(fd, buf, BUFFER_SIZE))
+    {
+        if(bytes_read == (size_t)-1)
+            handle_error("read failed");
+        if (!bytes_read)
+            break;
+
+        for(char *p = buf; (p = (char*) memchr(p, '\n', (buf + bytes_read) - p)); ++p)
+            ++lines;
+    }
+
+    return lines;
+}
+
