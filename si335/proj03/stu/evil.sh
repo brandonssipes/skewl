00b4581533438b0f3b546fcb7b5c535362fc03e1
Sipes <m195922@usna.edu>
2018-05-02 11:01:55 -0400

    adding in evil's calling process's pid to check

--- si335/proj03/stu/evil.sh
+++ si335/proj03/stu/evil.sh
@@ -2,11 +2,21 @@
 
 IFS=$'\n'
 aux=( $(ps aux) )
+ps=( $(ps -o pid,ppid) )
+
 
 declare -a worms
 declare -a PIDS
 count=0
 for i in ${aux[*]};do
+  if [[ $i = *"/bin/bash - ./evil.sh"* ]]; then
+    echo $i
+    IFS=$' '
+    line=( $i )
+    myPID=${line[1]}
+    myWorm=$(ps -o ppid= -p $myPID)
+    #echo $myWorm
+  fi
   if [[ $i = *"p3game"* ]]; then
     IFS=$' '
     line=( $i )
@@ -25,8 +35,9 @@ for i in ${aux[*]};do
   done
 done
 
-for i in ${PIDS[@]}; do
-  $(kill -2 $i)
-done
+echo $myPID
+#for i in ${PIDS[@]}; do
+#  $(kill -2 $i)
+#done
 
 
