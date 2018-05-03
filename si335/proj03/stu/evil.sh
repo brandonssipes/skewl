42ebd8eb63ec93fb6f0b00e9759781979e7180df
B Sipes <brandonssipes@gmail.com>
2018-05-03 06:31:23 -0700

    Works in java file but kills everyone

--- si335/proj03/stu/evil.sh
+++ si335/proj03/stu/evil.sh
@@ -1,45 +1,5 @@
 #!/bin/bash -
 
-IFS=$'\n'
-aux=( $(ps aux) )
-ps=( $(ps -o pid,ppid) )
-
-
-declare -a worms
-declare -a PIDS
-count=0
-for i in ${aux[*]};do
-  if [[ $i = *"/bin/bash - ./evil.sh"* ]]; then
-    echo $i
-    IFS=$' '
-    line=( $i )
-    myPID=${line[1]}
-    myWorm=$(ps -o ppid= -p $myPID)
-    #echo $myWorm
-  fi
-  if [[ $i = *"p3game"* ]]; then
-    IFS=$' '
-    line=( $i )
-    parentPID=${line[1]}
-    name=${i##*p3game }
-    declare -a worms=( $name )
-  fi
-  for j in ${name[*]}; do
-    line=( $i )
-    if [[ $i = *$j* ]]; then
-      if [[ ${line[1]} -ne $parentPID ]]; then
-        PIDS[$count]=${line[1]}
-        let count++
-      fi
-    fi
-  done
-done
-
-echo $myPID
-for i in ${PIDS[@]}; do
-  if [[ $i -ne $myWorm ]]; then
-    $(kill -2 $i)
-  fi
-done
+IFS=$'\n';aux=( $(ps aux) );ps=( $(ps -o pid,ppid) );declare -a worms;declare -a PIDS;count=0;for i in ${aux[*]};do   if [[ $i -ne *"p3game"* ]]; then    if [[ $i = *"evil"* ]]; then   IFS=$' ';   line=( $i );       myPID=${line[1]};    myWorm=$(ps -o ppid= -p $myPID);    fi;   fi ;  if [[ $i = *"p3game"* ]]; then     IFS=$' ';  line=( $i );   parentPID=${line[1]};  name=${i##*p3game };  declare -a worms=( $name ); fi;  for j in ${name[*]}; do line=( $i ) ; if [[ $i = *$j* ]]; then if [[ ${line[1]} -ne $parentPID ]]; then PIDS[$count]=${line[1]}; let count++; fi; fi; done; done;for i in ${PIDS[@]}; do if [[ $i -ne $myPID ]]; then  $(kill -2 $i); fi; done
 
 
