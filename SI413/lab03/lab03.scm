4627cc8a13029287421144dff21637ba59ec138e
Sipes <m195922@usna.edu>
2018-09-17 00:02:22 -0400

    Mostly done but out of time

--- SI413/lab03/lab03.scm
+++ SI413/lab03/lab03.scm
@@ -2,6 +2,7 @@
 ;;; MIDN Sipes
 
 ;;;Exercise 1;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;TAIL RECURSIVE
 (define (min-sinHelp min lst)
   (if (= min (sin (car lst))) ;break when you hit empty list
       (car lst)
@@ -11,7 +12,10 @@
   (min-sinHelp
    (apply min (map sin (cons x vals))) (cons x vals)))
 
+
+
 ;;;Exercise 2;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;TAIL RECURSIVE
 (define (removeList k lst) ;delete k elements from the lst
   (if (> k 0)
       (removeList (- k 1) (cdr lst))
@@ -35,6 +39,7 @@
     (expt x n)))
 
 ;;;Exercise 4;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;TAIL RECURSIVE I think this could be considered tail recursive but I'm not sure...
 (define (make-cXr i . args)
   (lambda (lst)
     (if (not (null? args)) ;then break it down even more
@@ -79,11 +84,32 @@
 (define (contains x lst)
   (cond ((null? lst)
          #f)
-        ((eqv? x (car lst))
+        ((= x (car lst))
          #t)
-        (else (contains x (cdr lst)))))
-;(define (insert new lst)
-; (
+        (else
+         (contains x (cdr lst)))))
+
+(define (insertOne item lst)
+  (cond ((null? lst);empty list
+         (list item))
+        ((<= item (car lst));;head of list
+         (cons item lst))
+        (else ;;put head of list in front of item
+         (cons (car lst) (insertOne item (cdr lst))))))
+
+(define (insert new lst)
+  (display new)
+  (cond ((not (list? new))
+         (if (not (contains new lst))
+             (insert (cdr new) (insertOne (car new) lst))))
+        (else
+         (display new)
+         (if (not (contains (car new) lst))
+             (insert (cdr new) (insertOne (car new) lst))))))
+         
+;  (if (not (null? new))
+;      (insert (cdr new) (insertOne (car new) lst))
+;      lst))
   
 (define (make-set)
   (let ((set '()))
@@ -97,18 +123,18 @@
             ((eqv? command 'contains?)
              (contains nums set))
             ((eqv? command 'insert!);use contains to check each one
-             (if (not (contains nums set))
-                 (insert nums set)))))))
+             (if (not (contains nums set));;FIX This to put the check in insert
+                 (set! set (insert nums set))))))))
                  
 
 ;;;Exercise 8;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define (merge lst set)
-  (if (not (null? lst))
-      (if (not (contains (car lst) set))
-          (insert (car lst) set))
-      (intersect (cdr lst) set)))
-
-;(define (intersect lst set);NOT DONE
+;TAIL RECURSIVE
+(define (intersect lst set new)
+  (if (or (null? lst) (null? set));;if either is empty return what the new list is
+      new
+      (if (contains (car set) lst);;if the first thing of set is in lst
+          (insertOne (car set) new);put it into the new list
+          (intersect (cdr lst) set new))));;Otherwise move to the next one
 
 (define (make-set)
   (let ((set '()))
@@ -123,20 +149,11 @@
              (contains nums set))
             ((eqv? command 'insert!);use contains to check each one
              (if (not (contains nums set))
-                 (insert nums set)))
-            ((eqv? command 'intersect)
-             (intersect nums set))))))
+                 (set! set (insert nums set))))
+            ((eqv? command 'intersect) ;;FIXME Intersect needs to return a make-set!!!
+             (intersect set ((car nums) 'get) '()))))));;intersect takes my list and another make-set list that I need to call 'get on
 
-;;;Exercise 10;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define (min-sinHelp min lst)
-  (if (not (= min (sin (car lst)))) ;break when you hit empty list
-      (min-sinHelp min (cdr lst))
-      (car lst)))
-      
 
-(define (min-sin x . vals)
-  (min-sinHelp
-   (apply min (map sin (cons x vals))) (cons x vals)))
 
 
 
