1d4974cfc9bfe2d0ec00f2f895f9384d1080a825
Sipes <m195922@usna.edu>
2018-09-18 10:32:28 -0400

    start of lab

---
+++ IC411/lab4/gonavy.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+#define NTHREADS 10
+void *army_function(void *);
+void *navy_function(void *);
+
+int statics[NTHREADS];      // will be initialized to 1,2,3...etc.
+int army_scores[NTHREADS];     // will be initialized to 0
+int navy_scores[NTHREADS];     // will be initialized to 0
+
+int main()
+{
+   // TODO: declare two arrays of threads, one each for army and navy threads.
+   //   Array size should be NTHREADS.
+
+
+   int i, j;
+   
+   srand(time(NULL));
+   
+   // Initialize the scores and the static numeric array
+   for(i=0; i < NTHREADS; i++)
+   {
+      statics[i] = i;
+      army_scores[i] = 0;
+      navy_scores[i] = 0;
+   }   
+
+    /* The statics[] array is useful because &i may increment
+    before it is referenced, resulting in the wrong thread_num being assigned.
+    We use &statics[i] instead of &i to initalize the (common) thread number. */
+    
+    // TODO: Create the Army threads. 
+
+
+
+    // TODO: Join all Army threads when complete
+
+  
+    
+    // TODO: Create the Navy threads. 
+
+
+
+    // TODO: Join all Navy threads when complete
+
+ 
+     
+  
+    // Reveal final scores
+    printf("Final scores:\n");
+    for(i=0; i < NTHREADS; i++)
+    {
+        printf("Thread:%d Navy:%d, Army:%d\n", i, navy_scores[i], army_scores[i]);
+    }    
+}
+
+void *army_function(void * arg)
+{
+    // thread_num is a "common" thread index between 0 and (NTHREADS-1)	
+    int thread_num = * (int *) arg;
+    
+    // TODO: Give Army a random integer score between 0 and 100
+
+}
+
+void *navy_function(void * arg)
+{
+	// thread_num is a "common" thread index between 0 and (NTHREADS-1)
+    int thread_num = * (int *) arg;
+    
+    // TODO: Give Navy double the score of Army for this thread
+
+}
