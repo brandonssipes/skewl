9bea2efa0c82894671dc96a33fbcea2c944777c4
Sipes <m195922@usna.edu>
2018-10-01 09:45:03 -0400

    not finished prime fac

--- SI413/proj1/proj.go
+++ SI413/proj1/proj.go
@@ -10,32 +10,34 @@ package main
 
 import "fmt"
 import "math/rand"//https://gobyexample.com/random-numbers
-
+//import "strings"//https://golang.org/pkg/strings/
 
 
 var bottlesOfBeer int = 0;
 
-//func primeFac (bottlesOfBeer int){
-//}
+func primeFac (bottlesOfBeer int){
 
-func takeOneBeer(ch chan bool) {
+}
+
+func takeOneBeer(ch chan bool, upTo int) {
     bottlesOfBeer+= rand.Intn(10);
-    go singTheSong(ch)
+    go singTheSong(ch, upTo)
 }
 
 func singTheSong(ch chan bool, upTo int) {
+    var facBeer = primeFac(bottlesOfBeer)
     if bottlesOfBeer < upTo {
         fmt.Printf("%d bugs in your code, %d bugs.\n", 
         bottlesOfBeer, bottlesOfBeer)
         fmt.Printf("Take one down and pass it around, %d bugs in your code.\n\n", bottlesOfBeer)
-        go takeOneBeer(ch)
+        go takeOneBeer(ch, upTo)
     } else if bottlesOfBeer == 1 {
         fmt.Println("1 bug in your code, 1 bug.")
         //fmt.Println("Take one down and pass it around, no more bottles of beer on the wall.\n")
-        go takeOneBeer(ch)
+        go takeOneBeer(ch, upTo)
     } else {
         //fmt.Println("No more bottles of beer on the wall, no more bottles of beer.")
-        fmt.Printf("Go to the store and buy some more beer, %d bugs in your code.\n", upTo)
+        fmt.Printf("Go to the store and buy some more tequila, %d bugs in your code.\n", upTo)
         ch <- true
     }
 }
