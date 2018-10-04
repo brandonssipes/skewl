c5aace6cac5dcd1285f1b527b8c0ea069e031061
Brandon Sipes <m195922@usna.edu>
2018-10-03 20:27:26 -0400

    added comments

--- SI413/proj1/proj.go
+++ SI413/proj1/proj.go
@@ -22,16 +22,23 @@ import "sync"
 var randNum int;
 var upTo int
 
+/*
+ * Takes an integer and produces its prime factorization.
+ */
 func primeFac (bottlesOfBeer int) string {
   var factors string = "("
   var i int
   var temp int = bottlesOfBeer
   i = 2
   for temp > 1 {
+	//Finds the first i that the current number (starting at the total bottles of beer) 
+	//is evenly divisible by and adds it to the string to be printed. 
     if temp % i == 0 {
       factors += strconv.Itoa(i)
+	  //That factor has to be removed from the total so that the next factor can be found.
       temp = temp/i
       if temp > 1 {
+		//Accounts for the final factor so an extra star is not printed at the end of the string.
         factors += "*"
       }
     }else{
@@ -42,6 +49,9 @@ func primeFac (bottlesOfBeer int) string {
   return factors
 }
 
+/*
+ * Prints out the "99 bottles of beer" song using the indicated file descriptor.
+ */
 func singTheSong(fd *os.File) {
     var bottlesOfBeer int = 1;
     for bottlesOfBeer < upTo {
@@ -59,29 +69,42 @@ func singTheSong(fd *os.File) {
 }
 
 func main() {
+	//Adds the current time as a seed for the random function
     rand.Seed(time.Now().UTC().UnixNano())//https://stackoverflow.com/questions/12321133/golang-random-number-generator-how-to-seed-properly
     randNum = rand.Intn(10);
     fmt.Printf("How many lines? ")
     fmt.Scanf("%d", &upTo)
 
+	//Provides synchronization functions for go routines (threads).
     var wg sync.WaitGroup //https://www.ardanlabs.com/blog/2014/01/concurrency-goroutines-and-gomaxprocs.html
-    wg.Add(2)
+    //Indicates that there will be 2 threads that must be waited on. Sets the WaitGroup counter to 2.
+	wg.Add(2)
 
+	//Executes the encapsulated code as a seperate thread. 
     go func(){
+	  //When the thread completes, decrements the WaitGroup counter by 1.
       defer wg.Done()
 
+	  //Creates a file called out.txt for writing.	
       f, err := os.OpenFile("out.txt", os.O_CREATE | os.O_WRONLY | os.O_TRUNC, 0666 ) //https://stackoverflow.com/questions/29981050/concurrent-writing-to-a-file
-      if err != nil {
+      //Error checking for file creation.
+	  if err != nil {
         panic(err)
       }
+	  
+	  //Prints the "99 Bottles of Beer" song to the file.
       singTheSong(f);
     }()
 
+	//Executes the encapsulated code as a seperate thread. 
     go func(){
+	  //When the thread completes, decrements the WaitGroup counter by 1.
       defer wg.Done()
 
+	  //Prints the "99 Bottles of Beer" song to Standard Out.
       singTheSong(os.Stdout);
     }()
 
+	//Waits until the WaitGroup counter is 0.
     wg.Wait()
 }
