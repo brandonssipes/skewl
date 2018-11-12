3510980c48b45f7c954f3f4ca5d77fd6fc809ff4
localadmin <localadmin@mac-lap34.local>
2018-11-12 15:53:43 -0500

    still working on it

--- SI413/lab09/builtins.hpp
+++ SI413/lab09/builtins.hpp
@@ -13,6 +13,7 @@
 // YOUR CODE HERE
 class Sqrt :public Stmt {
   private:
+  EXP* funexp;//the function 
   Exp* arg;
 
   public:
@@ -20,13 +21,23 @@ class Sqrt :public Stmt {
 
     }
 
-    void exec(Frame*ST)override{
+    void exec(Frame*ST)override{//This is an exec not an eval,  different?
       //Funcall gets the function
+      Closure func = ST.func();//funexp ->eval(ST).func();//get the closure
 
-
-      int x;
+      Frame* lamenv = new Frame(func.env);
+      lamenv->bind("ret",Value());
+      lamenv->bind(func.lam->getVar(), arg->eval(ST));
+      func.lam->getBody()->exec(lamenv);//This is what I'm doing
+      //return lamenv->lookup("ret");//Don't need this
+      //use ST to get the argument
+      //
+      //set the return value
+      int x = arg->eval(ST);
+      int x = func.lam->getVar();
       x = std::sqrt(x);
       //how do I return?
+      lamenv->rebind("ret",Value(x));
 
     }
 };
