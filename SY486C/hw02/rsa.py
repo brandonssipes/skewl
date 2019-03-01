825bc6605e05978bc9ec40072b286da6cc203208
Sipes <m195922@usna.edu>
2019-02-28 23:46:12 -0500

    3 of 4 parts done, only part 1 tested

--- SY486C/hw02/rsa.py
+++ SY486C/hw02/rsa.py
@@ -4,6 +4,7 @@
 #### last updated February 23, 2019##
 #####################################
 
+import secrets
 import sys
 
 class RSA:
@@ -12,7 +13,7 @@ class RSA:
             return this.__modPow(x,y,n)
         elif (y == -1):
             try:
-                g, a, _ = this.__invMod(x,n)
+                g, a, _ = this.__modInverse(x,n)
                 if g == 1:
                     return a % n
                 else:
@@ -30,7 +31,7 @@ class RSA:
             y = y/2 #shift y down by 1 bit
         return prod
     
-    def __invMod(this,a,b):
+    def __modInverse(this,a,b):
         #basically just do the extended euclid
         #format is (g,s,t) a*s + b*t = g = gcd(a,b)
         if b == 0:
@@ -41,6 +42,42 @@ class RSA:
             g, s, t = __invMod(b, r)
             return (g, t - s*q, s)
         
+
+    def MillerRabin(this,n):
+        # Determines if n is either composite or probably prime
+        # the error probability is .25 so we will run it about run times
+        # to get an error per centage of 2**-100
+        runs = 50
+        #input sanitization:
+        if(n%2 == 0) return "composite" #for dumb people
+        if(n <= 1) return "error"
+        #n - 1 = m*(2^k) (while m is odd)
+        k = 0
+        m = n-1 
+        while (m%2 == 0): #generate an m such that n -1 = m*(2^k) and m is odd
+            k+=1
+            m = m//2
+        #choose ran int a so that 1 <= a <= n -1
+        probability = 0
+        for _ in range(runs): #run test many times
+            a = secrets.below(n)#FIXME how many to get n???
+            a = random.randint(1,n-1)
+            #let b = a^m mod n
+            b = this.modPow(a,m,n)
+            if (b%n == 1):
+                #probably prime so continue checking
+                probability += 1
+                continue
+            for i in range(k):
+                if (b == (-1%n)):
+                    probability += 1
+                    break
+                else:
+                    b = this.modPow(b,2,n)
+        if (probability == runs) return "prime"
+        return "composite"
+
+
         
 
 
